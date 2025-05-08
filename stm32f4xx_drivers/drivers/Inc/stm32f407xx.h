5fa294217489b8926b57fe072ab7038468091bd5
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-05-08 08:48:49 -0500

    added I2C register definition

--- stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
@@ -192,6 +192,81 @@ typedef struct {
 } SPIx_RegDef_t;
 
 /***********************************************************/
+/************************** I2C ****************************/
+/***********************************************************/
+
+#define I2C1						((I2Cx_RegDef_t*)I2C1_BASEADDR)
+#define I2C2						((I2Cx_RegDef_t*)I2C2_BASEADDR)
+#define I2C3						((I2Cx_RegDef_t*)I2C3_BASEADDR)
+
+
+typedef struct{
+	__vo uint32_t CR1;					//I2C Control register
+	__vo uint32_t CR2;					//I2C Control register
+	__vo uint32_t OAR1;					//I2C Own address register
+	__vo uint32_t OAR2;					//I2C Own address register
+	__vo uint32_t DR;					//I2C Data register
+	__vo uint32_t SR1;					//I2C Status register
+	__vo uint32_t SR2;					//I2C Status register
+	__vo uint32_t CCR;					//I2C Clock control register
+	__vo uint32_t TRISE;				//I2C TRISE register
+	__vo uint32_t FLTR;					//I2C FLTR register
+}I2Cx_RegDef_t;
+
+//Bit Defintion
+
+#define I2C_CR1_PE					0
+#define I2C_CR1_SMBUS				1
+#define I2C_CR1_SMBTYPE				3
+#define I2C_CR1_ENARP				4
+#define I2C_CR1_ENPEC				5
+#define I2C_CR1_ENGC				6
+#define I2C_CR1_NOSTRETCH			7
+#define I2C_CR1_START				8
+#define I2C_CR1_STOP				9
+#define I2C_CR1_ACK					10
+#define I2C_CR1_POS					11
+#define I2C_CR1_PEC					12
+#define I2C_CR1_ALERT				13
+#define I2C_CR1_SWRST				15
+
+#define I2C_CR2_FREQ				0
+#define I2C_CR2_ITERREN				8
+#define I2C_CR2_ITEVTEN				9
+#define I2C_CR2_ITBUFEN				10
+#define I2C_CR2_DMAEN				11
+#define I2C_CR2_LAST				12
+
+#define I2C_SR1_SB					0
+#define I2C_SR1_ADDR				1
+#define I2C_SR1_BTF					2
+#define I2C_SR1_ADD10				3
+#define I2C_SR1_STOPF				4
+#define I2C_SR1_RXNE				6
+#define I2C_SR1_TXE					7
+#define I2C_SR1_BERR				8
+#define I2C_SR1_ARLO				9
+#define I2C_SR1_AF					10
+#define I2C_SR1_OVR					11
+#define I2C_SR1_PECERR				12
+#define I2C_SR1_TIMEOUT				14
+#define I2C_SR1_SMBALERT			15
+
+#define I2C_SR2_MSL					0
+#define I2C_SR2_BUSY				1
+#define I2C_SR2_TRA					2
+#define I2C_SR2_GENCALL				4
+#define I2C_SR2_SMBDEFAULT			5
+#define I2C_SR2_SMBHOST				6
+#define I2C_SR2_DUALF				7
+#define I2C_SR2_PEC					8
+
+#define I2C_CCR_CCR					0
+#define I2C_CCR_DUTY				14
+#define I2C_CCR_FS					15
+
+
+/***********************************************************/
 /************************* EXTI ****************************/
 /***********************************************************/
 
---
+++ stm32f4xx_drivers/drivers/Src/stm32f407xx_i2c_driver.c
@@ -0,0 +1,9 @@
+/*
+ * stm32f407xx_i2c_driver.c
+ *
+ *  Created on: May 8, 2025
+ *      Author: hugo-juarez
+ */
+
+#include "stm32f407xx_i2c_driver.h"
+
