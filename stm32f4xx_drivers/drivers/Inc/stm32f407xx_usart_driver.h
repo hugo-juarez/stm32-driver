000f28632daa26e2a18957a2680b87a52979356b
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-05-26 16:06:40 -0500

    initialize stm32 usart driver

---
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h
@@ -0,0 +1,13 @@
+/*
+ * sm32f407xx_usart_driver.h
+ *
+ *  Created on: May 26, 2025
+ *      Author: hugo-juarez
+ */
+
+#ifndef INC_STM32F407XX_USART_DRIVER_H_
+#define INC_STM32F407XX_USART_DRIVER_H_
+
+
+
+#endif /* INC_STM32F407XX_USART_DRIVER_H_ */
