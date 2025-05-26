6fe67ae1f413bef8aebb6fcab669097398b7b82b
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-05-26 17:23:43 -0500

    added usart register/configuration/handler structures

--- stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h
@@ -8,6 +8,29 @@
 #ifndef INC_STM32F407XX_USART_DRIVER_H_
 #define INC_STM32F407XX_USART_DRIVER_H_
 
+#include "stm32f407xx.h"
+
+/******************************************
+ *              CONFIGURATION
+ ******************************************/
+
+// --- Configuration Structure ---
+
+typedef struct {
+	uint8_t USART_Mode;
+	uint32_t USART_Baud;
+	uint8_t USART_StopBits;
+	uint8_t USART_Length;
+	uint8_t USART_Parity;
+	uint8_t USART_HWFlowControl;
+} USARTx_Config_t;
+
+// --- Handle Structure ---
+
+typedef struct {
+	USARTx_RegDef_t *pUSARTx;
+	USARTx_Config_t USART_Config;
+} USART_Handle_t;
 
 
 #endif /* INC_STM32F407XX_USART_DRIVER_H_ */
