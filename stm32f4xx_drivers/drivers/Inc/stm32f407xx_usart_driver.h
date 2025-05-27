7915edc5806fe6253068f4f4c2ba84b32f93ce3c
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-05-27 06:18:44 -0500

    added macros for handle config

--- stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx_usart_driver.h
@@ -15,22 +15,61 @@
  ******************************************/
 
 // --- Configuration Structure ---
-
 typedef struct {
 	uint8_t USART_Mode;
 	uint32_t USART_Baud;
 	uint8_t USART_StopBits;
-	uint8_t USART_Length;
+	uint8_t USART_WordLength;
 	uint8_t USART_Parity;
 	uint8_t USART_HWFlowControl;
 } USARTx_Config_t;
 
 // --- Handle Structure ---
-
 typedef struct {
 	USARTx_RegDef_t *pUSARTx;
 	USARTx_Config_t USART_Config;
 } USARTx_Handle_t;
+
+// --- @USART_Mode ---
+#define USART_MODE_ONLY_TX				0
+#define USART_MODE_ONLY_RX				1
+#define USART_MODE_TXRX					2
+
+// --- @USART_Baud ---
+#define USART_STD_BAUD_1200				1200
+#define USART_STD_BAUD_2400				2400
+#define USART_STD_BAUD_9600				9600
+#define USART_STD_BAUD_19200			19200
+#define USART_STD_BAUD_38400			38400
+#define USART_STD_BAUD_57600			57600
+#define USART_STD_BAUD_115200			115200
+#define USART_STD_BAUD_230400			230400
+#define USART_STD_BAUD_460800			460800
+#define USART_STD_BAUD_921600			921600
+#define USART_STD_BAUD_2M				2000000
+#define USART_STD_BAUD_3M				3000000
+
+// --- @USART-StopBits ---
+#define USART_STOPBITS_1				0
+#define USART_STOPBITS_0_5				1
+#define USART_STOPBITS_2				2
+#define USART_STOPBITS_1_5				3
+
+// --- @USART_WordLength ---
+#define USART_WORDLEN_8BITS				0
+#define USART_WORDLEN_9BITS				1
+
+// --- @USART_Parity ---
+#define USART_PARITY_DISABLE			0
+#define USART_PARITY_EVEN				1
+#define USART_PARITY_ODD				2
+
+// --- @USART_HWFlowControl ---
+#define USART_HW_FLOW_CTRL_NONE			0
+#define USART_HW_FLOW_CTRL_CTS			1
+#define USART_HW_FLOW_CTRL_RTS			2
+#define USART_HW_FLOW_CTRL_CTS_RTS		3
+
 
 /******************************************
  *              	APIS
@@ -61,5 +100,6 @@ void USART_IRQHandling(USARTx_Handle_t *pUSARTHandle);
 
 // --- Application Callback
 void USART_ApplicationEventCallback(USARTx_Handle_t *pUSARTHandle, uint8_t event);
+
 
 #endif /* INC_STM32F407XX_USART_DRIVER_H_ */
