67aec80932c8185b7a300a26c41335df15e4783f
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-06-11 05:09:57 -0500

    RTC test program

--- stm32f4xx_drivers/Src/rtc_lcd.c
+++ stm32f4xx_drivers/Src/rtc_lcd.c
@@ -5,7 +5,49 @@
  *      Author: hugo-juarez
  */
 
+#include "ds1307.h"
+#include <stdio.h>
+
 
 int main(void){
+
+	RTC_time_t time;
+	RTC_date_t date;
+
+	printf("RTC test \n");
+
+	if( ds1307_init() ){
+		printf("RTC Init has failed");
+		while(1);
+	}
+
+	date.day = WEDNESDAY;
+	date.date = 15;
+	date.month = 6;
+	date.year = 25;
+
+	time.hours = 5;
+	time.minutes = 25;
+	time.seconds = 41;
+	time.time_format = TIME_FORMAT_12HRS_AM;
+
+	ds1307_set_current_time(&time);
+	ds1307_set_current_date(&date);
+
+	ds1307_get_current_time(&time);
+	ds1307_get_current_date(&date);
+
+	char *am_pm;
+
+	if(time.time_format != TIME_FORMAT_24HRS){
+		am_pm = (time.time_format == TIME_FORMAT_12HRS_PM) ? "PM" : "AM";
+		printf("Current time = %s %s\n", time_to_string(&time), am_pm);
+	}else{
+		printf("Current time = %s\n", time_to_string(&time));
+	}
+
+	printf("Current date = %s <%s>\n", date_to_string(&date), get_day_of_week(date.day));
+
 	return 0;
 }
+
